6e51130f47ee7580940f816807f8f348f26dffc8
D3153 <82058076+D3153@users.noreply.github.com>
2022-11-09 09:34:43 -0500

    test

--- Opeq_CallCenter/Opeq_CallCenter/Opeq_CallCenter/Program.cs
+++ Opeq_CallCenter/Opeq_CallCenter/Opeq_CallCenter/Program.cs
@@ -1,15 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
+
 
 namespace Opeq_CallCenter
 {
-    internal class Program
+    internal static class Program
     {
-        static void Main(string[] args)
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
         {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
         }
     }
 }
