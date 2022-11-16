6e40df49981d38f9b0d81ed386d9954aa552b98e
Alihan Djamankulov <dubashevalihan@gmail.com>
2022-11-16 09:04:12 -0500

    Added Person Class

---
+++ Opeq_CallCenter/Opeq_CallCenter/Opeq_CallCenter/PersonClass.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Opeq_CallCenter
+{
+
+    interface PersonInterface
+    {
+        int Id
+        {
+            get; set;
+        }
+        String Name
+        {
+            get; set;
+        }
+
+    }
+
+
+    internal class PersonClass : PersonInterface
+    {
+        public int Id { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string Name { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }        
+    }
+}
