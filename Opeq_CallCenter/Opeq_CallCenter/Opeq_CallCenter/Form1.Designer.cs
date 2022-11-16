70b02aa8e8376e50b37f7e55d31b9531aed31e8c
Justom <cjustin1@outlook.com>
2022-11-15 20:49:36 -0500

    Added hint for Textbox and checking if Textbox is empty in login page

--- Opeq_CallCenter/Opeq_CallCenter/Opeq_CallCenter/Form1.Designer.cs
+++ Opeq_CallCenter/Opeq_CallCenter/Opeq_CallCenter/Form1.Designer.cs
@@ -41,9 +41,10 @@
             // 
             this.loginLabel.BackColor = System.Drawing.Color.Transparent;
             this.loginLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 45F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.loginLabel.Location = new System.Drawing.Point(499, 203);
+            this.loginLabel.Location = new System.Drawing.Point(374, 165);
+            this.loginLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.loginLabel.Name = "loginLabel";
-            this.loginLabel.Size = new System.Drawing.Size(236, 94);
+            this.loginLabel.Size = new System.Drawing.Size(177, 76);
             this.loginLabel.TabIndex = 0;
             this.loginLabel.Text = "Login";
             // 
@@ -53,9 +54,10 @@
             this.adminLoginLabel.BackColor = System.Drawing.Color.Transparent;
             this.adminLoginLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.adminLoginLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
-            this.adminLoginLabel.Location = new System.Drawing.Point(40, 689);
+            this.adminLoginLabel.Location = new System.Drawing.Point(30, 560);
+            this.adminLoginLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.adminLoginLabel.Name = "adminLoginLabel";
-            this.adminLoginLabel.Size = new System.Drawing.Size(175, 20);
+            this.adminLoginLabel.Size = new System.Drawing.Size(149, 17);
             this.adminLoginLabel.TabIndex = 1;
             this.adminLoginLabel.Text = "Etez-vous une admin?";
             this.adminLoginLabel.Click += new System.EventHandler(this.adminLoginLabel_Click);
@@ -64,23 +66,24 @@
             // 
             this.empNameTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.empNameTextBox.ForeColor = System.Drawing.SystemColors.ScrollBar;
-            this.empNameTextBox.Location = new System.Drawing.Point(396, 361);
-            this.empNameTextBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.empNameTextBox.Location = new System.Drawing.Point(297, 293);
+            this.empNameTextBox.Margin = new System.Windows.Forms.Padding(2);
             this.empNameTextBox.Multiline = true;
             this.empNameTextBox.Name = "empNameTextBox";
-            this.empNameTextBox.Size = new System.Drawing.Size(443, 37);
+            this.empNameTextBox.Size = new System.Drawing.Size(333, 31);
             this.empNameTextBox.TabIndex = 2;
-            this.empNameTextBox.Text = "Nom";
+            this.empNameTextBox.Text = "Entrer votre nom";
+            this.empNameTextBox.MouseClick += new System.Windows.Forms.MouseEventHandler(this.empNameTextBox_MouseClick);
+            //this.empNameTextBox.MouseEnter += new System.EventHandler(this.empNameTextBox_MouseEnter);
             // 
             // loginButton
             // 
             this.loginButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
             this.loginButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.loginButton.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.loginButton.Location = new System.Drawing.Point(531, 454);
-            this.loginButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.loginButton.Location = new System.Drawing.Point(398, 369);
             this.loginButton.Name = "loginButton";
-            this.loginButton.Size = new System.Drawing.Size(179, 52);
+            this.loginButton.Size = new System.Drawing.Size(134, 42);
             this.loginButton.TabIndex = 3;
             this.loginButton.Text = "Se Connecter";
             this.loginButton.UseVisualStyleBackColor = false;
@@ -90,25 +93,26 @@
             // 
             this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Location = new System.Drawing.Point(9, 10);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(374, 144);
+            this.pictureBox1.Size = new System.Drawing.Size(280, 117);
             this.pictureBox1.TabIndex = 4;
             this.pictureBox1.TabStop = false;
             // 
             // LoginForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
-            this.ClientSize = new System.Drawing.Size(1215, 750);
+            this.ClientSize = new System.Drawing.Size(911, 609);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.loginButton);
             this.Controls.Add(this.empNameTextBox);
             this.Controls.Add(this.adminLoginLabel);
             this.Controls.Add(this.loginLabel);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "LoginForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Login";
